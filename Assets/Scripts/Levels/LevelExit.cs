01414b5ac00b11cfc4004dd098df7d85cad447d9
GiBsyGG <jcval2801@gmail.com>
2023-09-15 23:43:56 -0500

    Add: Exit Sprites & Animation
    - Se añadieron los Sprites de la salida
    - Se añadió la animación para cambiar de nivel

--- Assets/Scripts/Levels/LevelExit.cs
+++ Assets/Scripts/Levels/LevelExit.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class LevelExit : MonoBehaviour
 {
-    private void EndLevel()
-    {
-          LevelManager.Instance.LoadNextLevel();
-    }
+     [SerializeField]
+     private Player _player;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
           if (collision.CompareTag("Player"))
           {
-               EndLevel();
+               StartCoroutine(EndLevel());
+               if(_player != null)
+                    _player.gameObject.SetActive(false);
           } 
     }
+
+     IEnumerator EndLevel()
+     {
+          if (transform.TryGetComponent(out Animator anim))
+          {
+               anim.SetTrigger("PortalIn");
+
+               // Ejecutar mientras la animación está en proceso
+               yield return new WaitForSeconds(1.3f);
+          }
+
+          LevelManager.Instance.LoadNextLevel();
+     }
 }
